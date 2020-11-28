7a403fa648a9ff7b0ce0ce0c38ce6593975d5cd5
plzprayme <dbfpzk142@gmail.com>
2020-11-28 18:21:28 +0900

    교수학과 데이터 넣기 완료

--- db/backup.sql
+++ db/backup.sql
@@ -1,11 +1,9 @@
 DROP DATABASE IF EXISTS `db_lecture`;
-
 CREATE DATABASE `db_lecture` DEFAULT CHARACTER SET utf8mb4;
-
 USE `db_lecture`;
 
-DROP TABLE IF EXISTS `students`;
 
+DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
   `id` INT(11) PRIMARY KEY,
   `grade` INT(11) NOT NULL,
@@ -18,7 +16,6 @@ CREATE TABLE `students` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `departments`;
-
 CREATE TABLE `departments` (
   `id` VARCHAR(5) PRIMARY KEY,
   `location` INT(6) NOT NULL,
@@ -29,18 +26,16 @@ CREATE TABLE `departments` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `professors`;
-
 CREATE TABLE `professors` (
-  `id` INT(11) PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `location` INT(11) UNIQUE NOT NULL,
-  `major` VARCHAR(255) NOT NULL,
-  `facility_name` VARCHAR(255) UNIQUE DEFAULT NULL
+  `id` INT(5) PRIMARY KEY,
+  `name` VARCHAR(10) NOT NULL,
+  `email` VARCHAR(30) NOT NULL,
+  `location` INT(6) UNIQUE NOT NULL,
+  `major` VARCHAR(30) NOT NULL,
+  `facility_name` VARCHAR(30) UNIQUE DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `lectures`;
-
 CREATE TABLE `lectures` (
   `id` INT(11) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
@@ -51,7 +46,6 @@ CREATE TABLE `lectures` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `facilities`;
-
 CREATE TABLE `facilities` (
   `name` VARCHAR(255) PRIMARY KEY,
   `position` INT(11) NOT NULL,
@@ -62,14 +56,12 @@ CREATE TABLE `facilities` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `professors_departments`;
-
 CREATE TABLE `professors_departments` (
-  `professor_id` INT(11) NOT NULL,
-  `department_id` VARCHAR(255) NOT NULL
+  `professor_id` INT(5) NOT NULL,
+  `department_id` VARCHAR(5) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 DROP TABLE IF EXISTS `students_lectures`;
-
 CREATE TABLE `students_lectures` (
   `student_id` INT(11) NOT NULL,
   `lecture_id` INT(11) NOT NULL
@@ -78,7 +70,9 @@ CREATE TABLE `students_lectures` (
 ALTER TABLE
   `students`
 ADD
-  FOREIGN KEY (`facility_name`) REFERENCES `facilities` (`name`) ON DELETE SET NULL;
+  FOREIGN KEY (`facility_name`) REFERENCES `facilities` (`name`) ON DELETE
+SET
+  NULL;
 
 ALTER TABLE
   `students`
@@ -130,9 +124,44 @@ ALTER TABLE
 ADD
   FOREIGN KEY (`facility_name`) REFERENCES `facilities` (`name`);
 
-  
-
-INSERT INTO 
-`departments` (`id`, `location`, `name`, `college`, `graduate_point`, `max_student`)
+-- 학과 생성 코드 
+INSERT INTO
+  `departments` (`id`, `location`, `name`, `college`, `graduate_point`, `max_student`)
 VALUES
-("ENG01", 90201, "컴퓨터공학과", "공과대학", 136, 60);
+  ("ENG01", 90201, "컴퓨터공학과", "공과대학", 126, 60),
+  ("ENG02", 90201, "컴퓨터통신무인기술학과", "공과대학", 136, 150),
+  ("LIB01", 40201, "영어영문학과", "문과대학", 120, 110),
+  ("EDU01", 30201, "수학교육과", "사범대학", 127, 25),
+  ("LAW01", 560501, "법학과", "법정대학", 120, 40),
+  ("INT01", 50201, "빅데이터응용학과", "사회적경제융합대학", 120, 60),
+  ("LSN01", 60201, "식품영양학과", "생명나노대학", 120, 60);
+
+-- 교수 생성 코드
+INSERT INTO
+  `professors` (`id`, `name`, `email`, `location`, `major`)
+VALUES
+  (10001, "최의인", "eichoi@hnu.kr", 90701, "데이터베이스"),
+  (10002, "이강수", "gslee@hnu.kr", 90702, "소프트웨어공학"),
+  (10003, "이만희", "manheelee@hnu.kr", 90703, "시스템보안"),
+  (10004, "안기영", "kya@hnu.kr", 90704, "프로그래밍언어"),
+  (10005, "소우영", "wsoh@hnu.kr", 90705, "뉴럴 네트워크"),
+  (10006, "변지현", "byunjh@hnu.kr", 40301, "영작 및 독해"),
+  (10007, "김화길", "hwakil@gmail.com", 30102, "해석학"),
+  (10008, "피용호", "drchopin@gmail.com", 560501, "노동법"),
+  (10009, "김명준", "mkim@hnu.kr", 50101, "빅데이터응용"),
+  (10010, "송수진", "sjsong9635@gmail.com", 60501, "영양학");
+
+-- 교수 학과 관계 매핑
+INSERT INTO
+  `professors_departments` (`professor_id`, `department_id`)
+VALUES
+  (10001, "ENG01"), (10001, "ENG02"),
+  (10002, "ENG01"), (10002, "ENG02"),
+  (10003, "ENG01"), (10003, "ENG02"),
+  (10004, "ENG01"), (10004, "ENG02"),
+  (10005, "ENG01"), (10005, "ENG02"),
+  (10006, "LIB01"),
+  (10007, "EDU01"),
+  (10008, "LAW01"),
+  (10009, "INT01"),
+  (10010, "LSN01");
