8706e485db411963bb6d5a0e52fa38e22deaaeb5
plzprayme <dbfpzk142@gmail.com>
2020-12-02 19:34:17 +0900

    upanddown custom ok

--- project1/upanddown.c
+++ project1/upanddown.c
@@ -3,25 +3,34 @@
 #define ITER 10
 #define PRODUCER_ITER ITER * 8 + 1
 #define CONSUMER_ITER ITER * 10
+
 void *thread_increment(void *arg);
 void *thread_decrement(void *arg);
+
 int x = -1; 
+
 pthread_mutex_t m;
 pthread_cond_t count_nonzero;
+
 int main() {
 
-  // input ITER NUMS, produce NUMS
+  int iter_nums;
+  int *p = &iter_nums;
+
+  printf("<< input iter nums: ");
+  scanf("%d", &iter_nums);
+
   pthread_t tid1, tid2;
   pthread_cond_init(&count_nonzero, NULL);
   pthread_mutex_init(&m, NULL);
 
-  pthread_create(&tid1, NULL, thread_increment, NULL);
-  pthread_create(&tid2, NULL, thread_decrement, NULL);
+  pthread_create(&tid1, NULL, thread_increment, p);
+  pthread_create(&tid2, NULL, thread_decrement, p);
   
   pthread_join(tid1, NULL);
   pthread_join(tid2, NULL);
 
-  if (x < 0 || x > 20)
+  if (x < 0 || x > 10)
     printf("ERROR! counter=%d\n", x);
   else
     printf("OK counter=%d\n", x);
@@ -33,21 +42,20 @@ int main() {
 }
 
 void * thread_increment (void *arg) {
-  int i, val;
+  int i;
   x = 1;
-  for (i=0; i<50; i++) {
+  for (i=0; i<*(int*)arg; i++) {
     pthread_mutex_lock(&m);
     
-    if (x==20) {
+    if (x==10) {
       pthread_cond_signal(&count_nonzero);
       pthread_cond_wait(&count_nonzero, &m);
-      x = 2;
+      x++;
     }
 
-    val = x;
-    printf("Producer>> %u:%d\n", (unsigned int) pthread_self(), val);
+    printf("Producer>> %u:%d\n", (unsigned int) pthread_self(), x);
 
-    x = val + 1;
+    ++x;
     pthread_mutex_unlock(&m);
   }
   pthread_cond_signal(&count_nonzero);
@@ -55,8 +63,8 @@ void * thread_increment (void *arg) {
 }
 
 void * thread_decrement (void *arg) {
-  int i, val;
-  for (i=0; i<50; i++) {
+  int i;
+  for (i=0; i<*(int*)arg; i++) {
     pthread_mutex_lock(&m);
 
     if (x<1) {
@@ -64,11 +72,10 @@ void * thread_decrement (void *arg) {
       pthread_cond_wait(&count_nonzero, &m);
     }
 
-
-    val = x;
-    printf("Consumer<< %u:%d\n", (unsigned int) pthread_self(), val);
-    x = val - 1;
+    printf("Consumer<< %u:%d\n", (unsigned int) pthread_self(), x);
+    --x;
     pthread_mutex_unlock(&m);
   }
+  pthread_cond_signal(&count_nonzero);
   pthread_exit(NULL);
 }
